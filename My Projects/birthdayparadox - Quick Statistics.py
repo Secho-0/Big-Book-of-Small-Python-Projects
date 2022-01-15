71d61d28404200786c0e0917c78e50d9f20532b9
IsAnEcho <48868031+Secho-0@users.noreply.github.com>
2022-01-14 17:12:00 -0700

    Update birthdayparadox - Quick Statistics.py

--- My Projects/birthdayparadox - Quick Statistics.py
+++ My Projects/birthdayparadox - Quick Statistics.py
@@ -9,27 +9,30 @@ import datetime as dt, random as r
 
 def getBirthdays(numberOfBirthdays):
     """Get a list of random birthdays """
-    birthdays = []
-    for i in range(numberOfBirthdays):
+    newBirthdays = []
+    for k in range(numberOfBirthdays):
         yearStart = dt.date(1994, 1, 1)  # Set start date
         someDays = dt.timedelta(r.randint(0, 364))  # add some days
         bday = yearStart + someDays  # now you have a birthday
 
-        birthdays.append(bday)  # add birthday to the list
+        newBirthdays.append(bday)  # add birthday to the list
 
-    return birthdays
+    return newBirthdays
 
 
 def getMatch(birthdayList):
-    """Get a birthday that is shared in the list"""
+    """Get birthdays that are shared in the list"""
     matchingBirthdays = []
     if len(birthdayList) == len(set(birthdayList)):
-        return None  # All birthdays Unique
+        return matchingBirthdays  # All birthdays Unique
 
     for a, birthdayA in enumerate(birthdayList):
         for b, birthdayB in enumerate(birthdayList[a + 1:]):
             if birthdayA == birthdayB:
-                return birthdayA  # return the matching birthday
+                if birthdayA not in matchingBirthdays:
+                    matchingBirthdays.append(birthdayA)
+    return matchingBirthdays  # return the matching birthday
+
 
 while True:
     print('''Birthday Paradox, by Al Sweigart al@inventwithpython.com
@@ -56,9 +59,11 @@ while True:
     print("-"*50)
 
     print('Here are', numBdays, 'birthdays:')
+
     birthdays = getBirthdays(numBdays)
+
     for i, birthday in enumerate(birthdays):
-        if i !=0:
+        if i != 0:
             print(", ", end='')
         mName = MONTHS[birthday.month - 1]
         dateText = f'{mName} {birthday.day}'
@@ -66,46 +71,62 @@ while True:
     print()
     print()
 
-    match = getMatch(birthdays)
+    match = list()
+    match.extend(getMatch(birthdays))
 
     print("In this simulation, ", end='')
 
-    if match is not None:
-        mName = MONTHS[birthday.month - 1]
-        dateText = f'{mName} {birthday.day}'
-        print("Multiple People have birthdays on, ", dateText)
+    if len(match) != 0:
+        for i, matchedBDay in enumerate(match):
+            if i != 0:
+                print(", ", end='')
+            mName = MONTHS[matchedBDay.month - 1]
+            dateText = f'{mName} {matchedBDay.day}'
+            print(dateText, end='')
     else:
         print("There are no matching birthdays")
     print()
 
-    #Run through 100k simulations
+    # Run through 100k simulations
     print("Generating a new batch of ", numBdays, "Random Birthdays 100,000 times...")
     input('Press Enter to begin')
 
-    simMatch = 0 # how many simulations had a matching birthday in them
-
+    simMatch = 0  # how many simulations had a matching birthday in them
+    numMatches = 0  # how many matches there were across all simulations
+    simNum = 100_000 * numBdays
     for i in range(100_000):
         # progress report every 10k
         if i % 10_000 == 0:
             print(i, 'simulations run so far.')
+
         birthdays = getBirthdays(numBdays)
 
+        match = list()
+
+        match.extend(getMatch(birthdays))
+
         # add to match counter
-        if getMatch(birthdays) is not None:
+        if len(match) != 0:
+            for m in match:
+                numMatches += 1
             simMatch += 1
     print('Done Running Simulations')
 
     # Display Results
-    probability = round((simMatch / 100_000 * 100), 2)
-    print('Out of 100,000 simulations of', numBdays, 'people, there was a')
-    print('matching birthday in that group', simMatch, 'times. This means')
-    print('that', numBdays, 'people have a', probability, '% chance of')
-    print('having a matching birthday in their group.')
-    print('That\'s probably more than you would think!')
-    print('\t Would you like to run another simulation? Y/N')
+    totalBDaysMade = numBdays * 100_000
+    probability_oneRound = round((simMatch / 100_000) * 100, 2)
+    probability_allSims = round(numMatches/totalBDaysMade * 100, 2)
+    print(f'''\tOut of 100,000 simulations there was a matching birthday in each group of {numBdays} people {simMatch} times. 
+    This means that out of {numBdays} people have a {probability_oneRound}% chance of having a matching birthday in the group each round.
+    That\'s probably more than you would think!
+    
+    In total there were {numMatches} matching birthdays found out of the {simNum} simulations run!
+    Which means you have a {probability_allSims}% chance of sharing a birthday in a group {totalBDaysMade} people big!
+    ''')
+    print('Would you like to run another simulation? Y/N')
     user_choice = input('> ')
     if not user_choice.lower().startswith('y') or user_choice.lower().startswith('n'):
-        user_choice = input("Enter a valid option\n> 80",)
+        user_choice = input("Enter a valid option\n> ",)
 
     elif user_choice.lower().startswith('y'):
         continue
